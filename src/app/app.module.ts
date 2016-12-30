9a0183c00e7543daebcd1735ae08a3b1c4363ee7
Steven Kauyedauty <skauyedauty@gmail.com>
2016-12-30 01:27:58 -0700

    setting up mean stack a2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { PostsService } from './posts.service';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES), // Add routes to the app
+    MaterialModule.forRoot()
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
